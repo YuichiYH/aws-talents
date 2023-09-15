f4c28fbcfd4f26e992f18917c210368962ccb534
YuichiYH <64170401+YuichiYH@users.noreply.github.com>
2023-09-15 15:34:45 -0300

    Remade script.js

--- src/script old.js
+++ src/script old.js
@@ -1,4 +1,3 @@
-const cvs = ["./public/images/Emily.png", "./public/images/Jennifer.png", "./public/images/Moises.png", "./public/images/Wilson.png", "./public/images/Yuichi.png"];
 const slider = document.querySelectorAll('.slider');
 const btnPrev = document.getElementById('prev-button');
 const btnNext = document.getElementById('next-button');
@@ -10,26 +9,26 @@ function hideSlider(){
 }
 
 function showSlider(){
-    slider[((currentSlide % 2) + 2) % 2].classList.add('on')
+    slider[currentSlide].classList.add('on')
 }
 
 function nextSlider(){
     hideSlider()
-    slider[(((currentSlide + 1) % 2) + 2) % 2].src = cvs[(((currentSlide + 1) % 5) + 5) % 5]
-
-    currentSlide++
-    
-    console.log(currentSlide)
+    if(currentSlide == slider.length -1) {
+        currentSlide = 0
+    } else {
+        currentSlide++
+    }
     showSlider()
 }
 
 function prevSlider(){
     hideSlider()
-    slider[(((currentSlide - 1) % 2) + 2) % 2].src = cvs[(((currentSlide - 1) % 5) + 5) % 5]
-
-    currentSlide--
-    
-    console.log(currentSlide)
+    if(currentSlide == 0) {
+        currentSlide = slider.length -1
+    } else {
+        currentSlide--
+    }
     showSlider()
 }
 
