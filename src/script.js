155567db5b6cf5a60ad34b45d2fb65dfdfe430c3
YuichiYH <64170401+YuichiYH@users.noreply.github.com>
2023-09-15 17:43:01 -0300

    Fixed footer

--- src/script.js
+++ src/script.js
@@ -1,37 +1,35 @@
 const cvs = ["./public/images/Emily.png", "./public/images/Jennifer.png", "./public/images/Moises.png", "./public/images/Wilson.png", "./public/images/Yuichi.png"];
-const slider = document.querySelectorAll('.slider');
+const slider = document.querySelector('.slider');
 const btnPrev = document.getElementById('prev-button');
 const btnNext = document.getElementById('next-button');
 
 let currentSlide = 0;
 
 function hideSlider(){
-    slider.forEach(item => item.classList.remove('on'))
+    slider.classList.remove('on')
 }
 
 function showSlider(){
-    slider[((currentSlide % 2) + 2) % 2].classList.add('on')
+    slider.classList.add('on')
 }
 
 function nextSlider(){
     hideSlider()
-    slider[(((currentSlide + 1) % 2) + 2) % 2].src = cvs[(((currentSlide + 1) % 5) + 5) % 5]
 
     currentSlide++
-    
-    console.log(currentSlide)
-    showSlider()
 }
 
 function prevSlider(){
     hideSlider()
-    slider[(((currentSlide - 1) % 2) + 2) % 2].src = cvs[(((currentSlide - 1) % 5) + 5) % 5]
 
     currentSlide--
-    
-    console.log(currentSlide)
-    showSlider()
 }
+
+slider.addEventListener("transitionend", () => {
+    slider.src = cvs[(((currentSlide) % 5) + 5) % 5]
+
+    showSlider()
+})
 
 btnNext.addEventListener('click', nextSlider)
 btnPrev.addEventListener('click', prevSlider)
